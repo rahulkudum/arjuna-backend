c8df0a76e477db27fdc8622f830048f66ba70ec4
rahulkudum <rahulkudum@gmail.com>
2021-05-22 15:29:33 +0530

    version 1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.all("*", function (req, res, next) {
  res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Credentials", true);
+
  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
