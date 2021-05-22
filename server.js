e8475678bfbc8e55ea4dca6ddc632505e920db4b
rahulkudum <rahulkudum@gmail.com>
2021-05-22 15:54:15 +0530

    version 1

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cors());
 
-app.all("*", function (req, res, next) {
+app.use((req, res, next) => {
  res.header("Access-Control-Allow-Origin", "*");
-
- res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
 });
 
