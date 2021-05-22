0e5002c0a3dfbde1f5540a8665212e319a9d526b
rahulkudum <rahulkudum@gmail.com>
2021-05-22 15:12:59 +0530

    version 1

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cors());
 
+app.all("*", function (req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Credentials", true);
+ res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
 const uri = "mongodb+srv://arjuna:arjuna@cluster0.ypjlp.mongodb.net/arjuna?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
