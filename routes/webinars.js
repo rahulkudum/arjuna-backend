9b86da930ec338bad97ed8ea30c9e4c2aa2c1661
rahulkudum <rahulkudum@gmail.com>
2021-05-23 00:36:27 +0530

    version 1

--- routes/webinars.js
+++ routes/webinars.js
@@ -266,7 +266,7 @@ var returnRouter = function (io) {
       count2++;
       if (count2 === webinar.users.length) {
        wbm
-        .start({ showBrowser: true })
+        .start()
         .then(async () => {
          //  console.log(qrCodeData);
          //  res.send(qrCodeData);
