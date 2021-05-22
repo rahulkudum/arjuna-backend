4351cbb0acba0a90af3e97971b250d98a70d15c7
rahulkudum <rahulkudum@gmail.com>
2021-05-22 15:59:47 +0530

    version 1

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-app.use(cors());
 
 app.use((req, res, next) => {
  res.header("Access-Control-Allow-Origin", "*");
