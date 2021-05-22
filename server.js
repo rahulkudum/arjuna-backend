359b3aa48624a0291467885e76366c72c0619010
rahulkudum <rahulkudum@gmail.com>
2021-05-22 19:16:44 +0530

    v1

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
-app.use((req, res, next) => {
- res.header("Access-Control-Allow-Origin", "https://arjunafe.herokuapp.com");
- next();
-});
+app.use(cors());
 
 const uri = "mongodb+srv://arjuna:arjuna@cluster0.ypjlp.mongodb.net/arjuna?retryWrites=true&w=majority";
 
@@ -33,3 +30,19 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
  console.log(`server is running on port `);
 });
+
+// const app = require("express")();
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http);
+
+// io.on("connection", (socket) => {
+//  socket.on("message", (req) => {
+//   console.log(req);
+//   io.emit("message", "response1");
+//   io.emit("message", "response2");
+//  });
+// });
+
+// http.listen(4000, function () {
+//  console.log("listening on port 4000");
+// });
